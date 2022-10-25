0ac08d23dd958d637a78a8a50d185f6fe30efad8
Scott Motte <mot@mot.la>
2022-10-25 10:52:54 -0700

    update requirements

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 # app/main.py
 import os
 from dotenv_vault import load_dotenv
+
+hello = os.getenv('HELLO')
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-  hello = os.getenv("HELLO")
   return f"<p>Hello, {hello}!</p>"

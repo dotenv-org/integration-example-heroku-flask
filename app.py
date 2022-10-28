7a9371bd94d4f5b422ddbde58cce863b38dadb75
Scott Motte <mot@mot.la>
2022-10-28 10:31:17 -0700

    Ready for heroku

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 # app/main.py
 import os
 from dotenv_vault import load_dotenv
-
-hello = os.getenv('HELLO')
-
 from flask import Flask
 
 app = Flask(__name__)
 
+load_dotenv()
+
 @app.route("/")
 def index():
+  hello = os.getenv("HELLO")
   return f"<p>Hello, {hello}!</p>"
